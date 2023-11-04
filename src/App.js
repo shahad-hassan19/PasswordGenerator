7b70d6db791ef4a50c96dcfd35a85ded656fe625
Shahad Hassan <shahadg1983@gmail.com>
2023-11-04 08:13:01 +0530

    Added logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useCallback} from 'react';
 
 function App() {
+
+  const [length, setLength] = useState('8');
+  const [numAllowed, setNumAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState()
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz";
+    if(numAllowed) str+= "0123456789";
+    if(charAllowed) str += "!£$%&*_~@?./";
+  },
+  [length, numAllowed, charAllowed, setPassword]);
+
   return (
     <div className="App">
-      <h1 className='text-4xl text-center'>Password Generator</h1>
+      <h1 className='text-4xl text-center text-white'>Password Generator</h1>
     </div>
   );
 }
